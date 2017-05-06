021997bcbe10cc3bbd5d37b8bde76aa53c93d531
kolyan <vkont4@gmail.com>
2017-05-06 13:43:14 +0300

    added tests

--- src/test/java/io/github/yashchenkon/test/JsonContentLoaderTest.java
+++ src/test/java/io/github/yashchenkon/test/JsonContentLoaderTest.java
@@ -1,7 +1,46 @@
 package io.github.yashchenkon.test;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
 /**
  * @author Mykola Yashchenko
  */
 public class JsonContentLoaderTest {
+
+    private static final String expected = "{\n" +
+            "  \"string\" : \"test\",\n" +
+            "  \"number\" : 1,\n" +
+            "  \"boolean\" : true\n" +
+            "}";
+
+    @Rule
+    public JsonContentLoader jsonContentLoader = new JsonContentLoader();
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowExceptionWhenFileDoesNotExist_Default() throws IOException {
+        jsonContentLoader.load();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowExceptionWhenFileDoesNotExist() throws IOException {
+        jsonContentLoader.load("test123");
+    }
+
+    @Test
+    public void shouldLoadDefaultFile() throws IOException {
+        String actual = jsonContentLoader.load();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldLoadFile() throws IOException {
+        String actual = jsonContentLoader.load("fileName");
+
+        Assert.assertEquals(expected, actual);
+    }
 }
--- src/test/java/io/github/yashchenkon/test/JsonContentVerifierTest.java
+++ src/test/java/io/github/yashchenkon/test/JsonContentVerifierTest.java
@@ -1,7 +1,23 @@
 package io.github.yashchenkon.test;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 /**
  * @author Mykola Yashchenko
  */
 public class JsonContentVerifierTest {
+
+    @Rule
+    public JsonContentVerifier jsonContentVerifier = new JsonContentVerifier();
+
+    @Test
+    public void shouldThrowExceptionWhenFileDoesNotExist() {
+
+    }
+
+    @Test
+    public void shouldVerifyEqualObjects() {
+
+    }
 }
