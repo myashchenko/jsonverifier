d289a8c9291f6decd75f34d3fb96fc8894ae7046
kolyan <vkont4@gmail.com>
2017-05-06 15:34:02 +0300

    added javadocs

--- src/main/java/io/github/yashchenkon/test/JsonContentLoader.java
+++ src/main/java/io/github/yashchenkon/test/JsonContentLoader.java
@@ -11,29 +11,79 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 
 /**
+ * JsonContentLoader is a rule for loading JSON files from the classpath.
+ * This rule builds a path to the file based on the invoked test.
+ * For example, this test loads JSON from the classpath resource "json/TestClass/test/request.json":
+ * <pre>
+ *     class TestClass {
+ *
+ *         &#064;Rule
+ *         public JsonContentLoader jsonContentLoader = new JsonContentLoader();
+ *
+ *         &#064;Test
+ *         public void test() {
+ *             String json = jsonContentLoader.load("request");
+ *         }
+ *     }
+ * </pre>
+ *
  * @author Mykola Yashchenko
  */
 public class JsonContentLoader extends TestWatcher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonContentLoader.class);
 
+    /**
+     * default file name {@value} to use for {@link #load()}
+     */
     private static final String DEFAULT_FILE_NAME = "request";
+
+    /**
+     * template of the path to the file
+     */
     private static final String PATH = "json/%s/%s/%s.json";
 
+    /**
+     * current test method name
+     */
     private String methodName;
 
+    /**
+     * current test class
+     */
     private Class<?> testClass;
 
+    /**
+     * sets test class and method name to build path to file
+     *
+     * @param description description of the current test method
+     */
     @Override
     protected void starting(Description description) {
         this.testClass = description.getTestClass();
         this.methodName = description.getMethodName();
     }
 
+    /**
+     * loads json file from classpath
+     * uses {@link #load(String)} with default file name
+     *
+     * @return loaded json as string
+     * @throws IOException          if an I/O error occurs
+     * @throws NullPointerException if the file doesn't exist
+     */
     public String load() throws IOException {
         return load(DEFAULT_FILE_NAME);
     }
 
+    /**
+     * loads json file from classpath
+     *
+     * @param fileName name of the file without format (e.g "request" not "request.json")
+     * @return loaded json as string
+     * @throws IOException          if an I/O error occurs
+     * @throws NullPointerException if the file doesn't exist
+     */
     public String load(String fileName) throws IOException {
         String path = String.format(PATH, testClass.getSimpleName(), methodName, fileName);
         LOGGER.info("Reading file: {}", path);
--- src/main/java/io/github/yashchenkon/test/JsonContentVerifier.java
+++ src/main/java/io/github/yashchenkon/test/JsonContentVerifier.java
@@ -15,36 +15,94 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 
 /**
+ * JsonContentVerifier is a rule to check JSON returned by services. It compares
+ * JSON to files which are placed into classpath. The path to the file builds
+ * automatically based on the test class name and test method name.
+ *
  * @author Mykola Yashchenko
  */
 public class JsonContentVerifier extends TestWatcher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonContentVerifier.class);
 
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+    /**
+     * default JSON {@link ObjectMapper}
+     */
+    private static final ObjectMapper DEFAULT_MAPPER = new ObjectMapper();
+
+    /**
+     * default file name {@value} to use for {@link #assertJson(String)}
+     */
     private static final String DEFAULT_FILE_NAME = "response";
+
+    /**
+     * template of the path to the file
+     */
     private static final String PATH = "json/%s/%s/%s.json";
 
+    /**
+     * JSON {@link ObjectMapper} to deserialize JSON into {@link JsonNode}
+     */
+    private ObjectMapper objectMapper = DEFAULT_MAPPER;
+
+    /**
+     * current test method name
+     */
     private String methodName;
 
+    /**
+     * current test class
+     */
     private Class<?> testClass;
 
+    /**
+     * Creates instance of rule with custom {@link ObjectMapper}
+     * @param objectMapper custom {@link ObjectMapper}
+     * @return instance of the rule
+     */
+    public static JsonContentVerifier from(ObjectMapper objectMapper) {
+        JsonContentVerifier jsonContentVerifier = new JsonContentVerifier();
+        jsonContentVerifier.objectMapper = objectMapper;
+        return jsonContentVerifier;
+    }
+
+    /**
+     * sets test class and method name to build path to file
+     *
+     * @param description description of the current test method
+     */
     @Override
     protected void starting(Description description) {
         this.testClass = description.getTestClass();
         this.methodName = description.getMethodName();
     }
 
+    /**
+     * compare JSON to the content of the default file
+     *
+     * @param actualJson JSON to compare
+     * @throws IOException          if an I/O error occurs
+     * @throws NullPointerException if the file doesn't exist
+     * @throws AssertionFailedError if actualJson is not equal to the content of the default file
+     */
     public void assertJson(String actualJson) throws IOException {
         assertJson(actualJson, DEFAULT_FILE_NAME);
     }
 
+    /**
+     * compare JSON to the content of the specified file
+     *
+     * @param actualJson        JSON to compare
+     * @param expectedFileName  file name from which to load expected content
+     * @throws IOException          if an I/O error occurs
+     * @throws NullPointerException if the file doesn't exist
+     */
     public void assertJson(String actualJson, String expectedFileName) throws IOException {
         String path = String.format(PATH, testClass.getSimpleName(), methodName, expectedFileName);
         try (InputStream resourceAsStream = testClass.getClassLoader().getResourceAsStream(path)) {
             String expectedJson = IOUtils.toString(resourceAsStream, Charset.defaultCharset());
-            JsonNode actual = MAPPER.readTree(actualJson);
-            JsonNode expected = MAPPER.readTree(expectedJson);
+            JsonNode actual = objectMapper.readTree(actualJson);
+            JsonNode expected = objectMapper.readTree(expectedJson);
 
             LOGGER.info("EXPECTED JSON: " + expectedJson);
             LOGGER.info("ACTUAL JSON: " + actualJson);
