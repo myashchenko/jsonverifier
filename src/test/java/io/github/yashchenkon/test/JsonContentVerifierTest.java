c7b9a1521d1877c7aafef539db37bca9bea653cf
kolyan <vkont4@gmail.com>
2017-05-08 22:43:27 +0300

    added json content verifier tests

--- src/test/java/io/github/yashchenkon/test/JsonContentVerifierTest.java
+++ src/test/java/io/github/yashchenkon/test/JsonContentVerifierTest.java
@@ -3,21 +3,29 @@ package io.github.yashchenkon.test;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.IOException;
+
 /**
  * @author Mykola Yashchenko
  */
 public class JsonContentVerifierTest {
 
+    private static final String actualJson = "{\n" +
+            "  \"string\" : \"test\",\n" +
+            "  \"number\" : 1,\n" +
+            "  \"boolean\" : true\n" +
+            "}";
+
     @Rule
     public JsonContentVerifier jsonContentVerifier = new JsonContentVerifier();
 
-    @Test
-    public void shouldThrowExceptionWhenFileDoesNotExist() {
-
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowExceptionWhenFileDoesNotExist() throws IOException {
+        jsonContentVerifier.assertJson(actualJson);
     }
 
     @Test
-    public void shouldVerifyEqualObjects() {
-
+    public void shouldVerifyEqualObjects() throws IOException {
+        jsonContentVerifier.assertJson(actualJson, "fileName");
     }
 }
